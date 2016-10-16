c2ff4b0a193eadaaa266c24345e3d0a006e1fecc
Zac Arellano <zaccc@email.arizona.edu>
2016-10-15 17:50:46 -0700

    updated app.js (just for testing on node//express server)

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
 var express = require('express')
-var stylus = require('stylus')
 var app = express()
 
 app.use(express.static('build'))
-app.get('/', (req, res) => res.send('ayyy homie . . .'))
-
 app.listen(9000)
-module.exports = app
 
-// app.use(stylus.middleware({
-//   src: __dirname + '/app/css',
-//   dest: __dirname + '/build/css',
-//   compile: function compile(str, path){
-//      return stylus(str).set('filename', path).set('compress', true);
-//   }
-// }))
+module.exports = app
