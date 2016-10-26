02f2754b4f7173d7e1477fe53289fe5b91f2b91f
Zac Arellano <zaccc@email.arizona.edu>
2016-10-25 21:01:27 -0700

    changed "/*" to "*" on watching all index.html files

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 var express = require('express')
 var app = express()
 
-app.use(express.static('app'))
-app.get('/*', function(req, res) {
-  res.sendFile(__dirname + '/index.html')
+app.use(express.static(__dirname + '/app'))
+
+app.get('*', function(req, res) {
+  res.sendFile('index.html')
 })
+
 app.listen(9000, function() {
   console.log('app listening on port 9000')
 })
+
 module.exports = app
