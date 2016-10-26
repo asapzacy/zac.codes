9f23a66ed9cd9f2f9d760bcf90361614e664e555
Zac Arellano <zaccc@email.arizona.edu>
2016-10-25 17:50:52 -0700

    made the about + projects page their own index.html file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var express = require('express')
 var app = express()
 
-app.use(express.static('build'))
+app.use(express.static('app'))
 app.listen(9000)
 
 module.exports = app
--- app/js/main.js
+++ app/js/main.js
@@ -1,43 +1,23 @@
-
 (function() {
 
   // pixels + findArea function
   const code = document.querySelector('.js_pixels')
   const findArea = () => {
-    let area = window.innerWidth * window.innerHeight
-    area = String(area).replace(/(?=(?:\d{3})+\b)(?!\b)/g,',')
-    code.textContent = area
+    if (code && code.textContent !== null) {
+      let area = window.innerWidth * window.innerHeight
+      area = String(area).replace(/(?=(?:\d{3})+\b)(?!\b)/g,',')
+      code.textContent = area
+    }
   }
-  findArea()
+  // window event listeners for page load + page resize
+  window.addEventListener('load', findArea, { passive: true })
   window.addEventListener('resize', findArea, { passive: true })
-
-  // change logo img to inverted
-  const img = document.querySelector('.logo').children[0].src.split('.')
-  console.log(img)
-
 
   // menu button + navigation
   const header = document.querySelector('header')
   const menuBtn = document.querySelector('.js_menu-btn')
   menuBtn.addEventListener('click', function() {
     header.classList.toggle('menu-open')
-  })
-  const navItems = document.querySelectorAll('.menu-nav a')
-  const navItemsSize = navItems.length
-  for (let i = 0; i < navItemsSize; i++) {
-    navItems[i].addEventListener('click', function() {
-      header.classList.remove('menu-open')
-    })
-  }
-
-  // display correct page
-  window.addEventListener('hashchange', function(e) {
-    const oldPage = document.getElementById(e.oldURL.split('#')[1] || 'landing')
-    const newPage = document.getElementById(e.newURL.split('#')[1])
-    if (oldPage !== newPage) {
-      oldPage.classList.remove('page-shown')
-      newPage.classList.add('page-shown')
-    }
   })
 
 })()
