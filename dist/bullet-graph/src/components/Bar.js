04e19b5bf7ea479d0b0285b66dc8e4c2d93f9ca7
asapzacy <zacdarellano@gmail.com>
2017-11-26 07:43:46 +0000

    move directories

--- dist/bullet-graph/src/components/Bar.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Bar({ x, y, width, height, fill }) {
-  return (
-    <rect className='bar' x={x} y={y} width={width} height={height} fill={fill} />
-  )
-}
--- dist/bullet-graph/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Header() {
-  return (
-    <header className='headerContainer'>
-      <h1 className='header'>{'Bullet Graph'}</h1>
-    </header>
-  )
-}
