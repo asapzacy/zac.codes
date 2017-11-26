fe2745ea0bfb64ba8a2a2554e8a089894a89394f
asapzacy <zacdarellano@gmail.com>
2017-11-26 07:43:46 +0000

    move directories

--- dist/bullet-graph/src/helpers/utils.js
+++
@@ -1,8 +0,0 @@
-
-//  get bar width + height --> <Bar /> component
-export const getBarWidth = (width, fullWidth) => (width / fullWidth) === 0 ? 0 : `${(width / fullWidth) * 100}%`
-export const getBarHeight = (height, fullHeight) => fullHeight * height
-
-//  get X + Y position --> <Bar />, <Interval />, <Line />, <Text /> components
-export const getXPosition = (barWidth, fullWidth) => (barWidth / fullWidth) === 0 ? 0 : `${(barWidth / fullWidth) * 100}%`
-export const getYPosition = (barHeight, fullHeight) => (fullHeight - barHeight) / 2
--- dist/bullet-graph/src/tests/test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from '../containers/AppContainer'
-
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  render(<AppContainer />, div)
-})
